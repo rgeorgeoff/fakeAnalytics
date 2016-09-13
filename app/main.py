312191bb7cb3df9dd1b765b356ba32207a7c8b23
rgeorgeoff <richard@sgn.com>
2016-09-13 00:46:48 -0700

    fixed bugs, removed clutter

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, g, request, render_template, Response
-from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
-from os import path
+from flask import Flask
 import json
 
 app = Flask(__name__)
@@ -11,28 +9,18 @@ def respond():
     try:
         return json.dumps("recieved get")
     except ClientException as e:
-        return json.dumps(dict(error=e.message)), 400
+        return json.dumps(dict(error=e.message)), 404
+
+@app.route('/test')
+def respond_test():
+    return json.dumps("recieved test get")
 
 @app.route('/', methods=['POST'])
 def respond_post():
     try:
         return json.dumps("recieved post")
     except ClientException as e:
-        return json.dumps(dict(error=e.message)), 400
-
-@app.errorhandler(400)
-def bad_request(error):
-    from werkzeug.exceptions import BadRequestKeyError
-    if isinstance(error, BadRequestKeyError):
-        return serve_json(error.code, error="Missing field '%s'" % error.message)
-    if isinstance(error, ClientException):
-        return serve_json(error.code, error=error.description)
-    return serve_json(error.code, error=str(type(error)))
-
-@app.errorhandler(404)
-@app.errorhandler(500)
-def err_handler(error):
-    return serve_json(error.code, error=error.description)
+        return json.dumps(dict(error=e.message)), 404
 
 if __name__ == '__main__':
     app.run()
